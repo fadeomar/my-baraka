65d610ed442462c88a644e65a4ce9881f810d520
fadi omar <fadeomar2015@gmail.com>
2019-08-30 21:21:21 +0300

    create connection file using pg module to create new pool that take optins with db link

--- src/models/config/connection.js
+++ src/models/config/connection.js
@@ -1,1 +1,18 @@
-// connetin here
+const { Pool } = require("pg");
+
+require("env2")("config.env");
+
+let dbURL = "";
+
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV) {
+  dbURL == process.env.DATABASE_URL;
+} else {
+  dbURL = process.env.DEV_DB_URL;
+}
+
+const opt = {
+  connectionString: dbURL,
+  ssl: true
+};
+
+module.exports = new Pool(opt);
