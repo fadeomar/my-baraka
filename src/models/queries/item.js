02584970de1ce5096c815fc89a96b3a19b1fc509
fadi omar <fadeomar2015@gmail.com>
2019-08-31 11:34:22 +0300

    install jsonwebtoken and create home route and using verfiy from jsonweb token to get the username and user id from cookies and using format helper that return a single array contain lists and thier items

--- src/models/queries/item.js
+++ src/models/queries/item.js
@@ -1,14 +1,14 @@
 const { query } = require("../config/connection");
 
-const selectaAll = () => {
-  const sql = `SELECT * FROM baraka_item`;
+const selectAll = () => {
+  const sql = `SELECT * FROM baraka_item;`;
   return query(sql);
 };
 
-const isert = itemData => {
+const insert = itemData => {
   const { content, isDone, listId, userId } = itemData;
   const sql = {
-    text: `INSERT INTO baraka_item (content, is_done, list_id, user_id)`,
+    text: `INSERT INTO baraka_item (content, is_done, list_id, user_id) VALUES ($1,$2,$3,$4);`,
     values: [content, isDone, listId, userId]
   };
   return query(sql);
@@ -16,7 +16,7 @@ const isert = itemData => {
 
 const doneKey = id => {
   const sql = {
-    text: `UPDATE baraka_item SET is_done = true where id =$1`,
+    text: `UPDATE baraka_item SET is_done = true where id =$1;`,
     values: [id]
   };
   return query(sql);
@@ -26,3 +26,5 @@ const getItmesOwners = () => {
   const sql = `SELECT baraka_item.id, content, is_done, baraka_user.username from baraka_item INNER JOIN baraka_user ON baraka_user.id = baraka_item.user_id;`;
   return query(sql);
 };
+
+module.exports = { getItmesOwners, insert, selectAll, doneKey };
