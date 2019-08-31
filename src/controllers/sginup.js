e7eb5a05912bc17ed144a2de833053fc0cae152a
fadi omar <fadeomar2015@gmail.com>
2019-08-31 12:41:54 +0300

    create sginup route

--- src/controllers/sginup.js
+++ src/controllers/sginup.js
@@ -1,1 +1,29 @@
 const { hash } = require("bcrypt");
+
+const { sginupV, errorFeedback } = require("../validation");
+const { find, insert } = require("../models/queries/user");
+
+const get = (req, res) => {
+  res.render("sginup", { title: "Sgin Up" });
+};
+
+const post = (req, res, next) => {
+  const { email, username, password, confirmpassword } = req.body;
+  sginupV({ email, username, password, confirmpassword })
+    .then(() => find(username))
+    .then(result => {
+      if (result.rows.length !== 0)
+        throw Error("this username are already exists");
+    })
+    .then(() => hash(password, 10));
+  then(hased => insert({ email, username, password: hased }))
+    .then(() => res.redirect("/login"))
+    .catch(err => {
+      if (err.isJoi) res.send(errorFeedback(err.details[0].type));
+      else if (err.massage === "this username are already exists")
+        res.send(errorFeedback(err.massage));
+      else next(err);
+    });
+};
+
+module.exports = { get, post };
